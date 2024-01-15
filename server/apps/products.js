291453330077116e10e35c79e82daf7e129b4c01
SamitaTT <m.samita.tt@gmail.com>
2024-01-15 12:47:11 +0700

    refactor: add error response

--- server/apps/products.js
+++ server/apps/products.js
@@ -5,60 +5,82 @@ import { ObjectId } from "mongodb";
 const productRouter = Router();
 
 productRouter.get("/", async (req, res) => {
-  const collection = db.collection("products");
+  try {
+    const collection = db.collection("products");
 
-  const products = await collection.find({}).limit(10).toArray();
+    const products = await collection.find({}).limit(10).toArray();
 
-  return res.json({ data: products });
+    return res.json({ data: products });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 productRouter.get("/:productId", async (req, res) => {
-  const collection = db.collection("products");
-  const productId = new ObjectId(req.params.productId);
-  const product = await collection.findOne({
-    _id: productId,
-  });
-  return res.json({ data: product });
+  try {
+    const collection = db.collection("products");
+    const productId = new ObjectId(req.params.productId);
+    const product = await collection.findOne({
+      _id: productId,
+    });
+    return res.json({ data: product });
+  } catch (error) {
+    return res.status(404).json({ message: "Product Not Found" });
+  }
 });
 
 productRouter.post("/", async (req, res) => {
-  const collection = db.collection("products");
+  try {
+    const collection = db.collection("products");
 
-  const productData = { ...req.body };
-  const products = await collection.insertOne(productData);
+    const productData = { ...req.body };
+    const products = await collection.insertOne(productData);
 
-  return res.json({
-    message: `Product has been created successfully`,
-  });
+    return res.json({
+      message: `Product has been created successfully`,
+    });
+  } catch (error) {
+    return res.status(400).json({ message: "That's an Error" });
+  }
 });
 
 productRouter.put("/:productId", async (req, res) => {
-  const collection = db.collection("products");
-  const productId = new ObjectId(req.params.productId);
-  const newProductData = { ...req.body };
-  await collection.updateOne(
-    {
-      _id: productId,
-    },
-    {
-      $set: newProductData,
-    }
-  );
+  try {
+    const collection = db.collection("products");
+    const productId = new ObjectId(req.params.productId);
+    const newProductData = { ...req.body };
+    await collection.updateOne(
+      {
+        _id: productId,
+      },
+      {
+        $set: newProductData,
+      }
+    );
 
-  return res.json({
-    message: `Product has been updated successfully`,
-  });
+    return res.json({
+      message: `Product has been updated successfully`,
+    });
+  } catch (error) {
+    return res.status(404).json({ message: "Product Not Found" });
+  }
 });
 
 productRouter.delete("/:productId", async (req, res) => {
-  const collection = db.collection("products");
-  const productId = new ObjectId(req.params.productId);
-  await collection.deleteOne({
-    _id: productId,
-  });
-  return res.json({
-    message: `Product has been deleted successfully`,
-  });
+  try {
+    const collection = db.collection("products");
+    const productId = new ObjectId(req.params.productId);
+    await collection.deleteOne({
+      _id: productId,
+    });
+    return res.json({
+      message: `Product has been deleted successfully`,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Cannot delete product because DB failed" });
+  }
 });
 
 export default productRouter;
