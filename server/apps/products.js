77d90d96e3885093c231d353941ed25db5fcf8e0
Gravizz <peeke0rr@gmail.com>
2024-01-15 14:33:35 +0700

    feat:add post api

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { db } from "../utils/db.js";
+import { ObjectId } from "mongodb";
 
 const productRouter = Router();
 
@@ -6,7 +8,18 @@ productRouter.get("/", (req, res) => {});
 
 productRouter.get("/:id", (req, res) => {});
 
-productRouter.post("/", (req, res) => {});
+productRouter.post("/", async (req, res) => {
+  try {
+    const collection = db.collection("products");
+    const productData = { ...req.body, created_at: new Date() };
+    const newProductData = await collection.insertOne(productData);
+    return res.json({
+      message: `Product Id ${newProductData.insertedId} has been created successfully`,
+    });
+  } catch (error) {
+    message: `${error}`;
+  }
+});
 
 productRouter.put("/:id", (req, res) => {});
 
